2b910b4018f351d4e1306ecac8be298f9136ab5b
JoePasquale404 <aleksandrsemenov5256@gmail.com>
2026-01-09 13:19:19 +0900

    Add files via upload

---
+++ lesson 10/main.js
@@ -0,0 +1,23 @@
+// 1 задача
+
+let firtsName = "Sanek";
+let lastName = "Semenov";
+let isStudent = true;
+
+// 2 задача
+
+let age = 31;
+let currentYear = 2026;
+let birthYear = currentYear - age ;
+console.log(birthYear)
+
+
+// 3 задача
+
+console.log(`Меня зовут ${firtsName} ${lastName}, мне ${age} лет. Я ученик${isStudent ? "" : " не"} курса: ${isStudent}.`);
+
+
+
+// 4 задача
+
+РЕЗУЛЬТАТ: '123456789falsetrue'
